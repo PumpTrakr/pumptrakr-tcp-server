9113c5f20d305d2ba3781179e3d70ebe8a04aa33
Alan Long <alan.d.long@outlook.com>
2020-10-12 15:19:33 -0500

    Remove empty and nil string check

--- tcp_server.rb
+++ tcp_server.rb
@@ -27,7 +27,7 @@ class ProxyServer
     request = connection.gets
     # connection.close if request.nil?
     puts(request)
-    post_to_server(request) unless empty_string?(request) || request.nil?
+    post_to_server(request)
   end
 
   def empty_string?(str)
@@ -38,17 +38,17 @@ class ProxyServer
     puts msg
     # Create the request object to use
     uri, request = generate_http_obj(msg)
-  
+
     # Set the options
     req_options = {
       use_ssl: uri.scheme == 'https'
     }
-  
+
     # Make the call
     response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
       http.request(request)
     end
-  
+
     handle_response(response)
   end
 
@@ -57,13 +57,13 @@ class ProxyServer
     protocol = 'https://'
     host = 'api.pumptrakr.com'
     path = '/api/v1/webhooks/tcp_proxy'
-  
+
     uri = URI.parse("#{protocol}#{host}#{path}")
-  
+
     request = Net::HTTP::Post.new(uri)
     request.content_type = 'application/json; charset=utf-8'
     request.body = data_prep(msg)
-  
+
     [uri, request]
   end
 
@@ -73,7 +73,7 @@ class ProxyServer
 
   def handle_response(response)
     puts response&.code
-  
+
     # Check the status code
     if %w([200 201 204]).include? response.code
       # Everything worked

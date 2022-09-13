6bf4db6b871255cf6b62f54d3b2cdd8bebe3c2f6
Lee Hodges <lee@codefiworks.com>
2022-09-13 13:50:07 -0500

    Masquerade
    Masquerade as a device to send response messages to server

---
+++ proxy_test_msg.rb
@@ -0,0 +1,120 @@
+# frozen_string_literal: true
+
+require 'logger'
+require 'byebug'
+
+# Define our ProxyServer
+class ProxyTestMsg
+  def initialize(port, environment, module_model, msg)
+    @environment = environment
+    @model = module_model
+    @server = TCPServer.new(port)
+    @msg = msg
+    puts "hit initialize"
+    # @log = Logger.new("log-#{@environment}-#{@model}.txt", 4, 1_024_000)
+    # @log.debug "Listening on port #{port}\nReceived messages will be delivered to: \n#{generate_uri}"
+  end
+
+  def start
+    puts "Starting server on port"
+    byebug
+    Socket.accept_loop(@server) do |connection|
+      Thread.current.abort_on_exception = false
+      Thread.new do
+
+        loop do
+          handle(connection, Time.now.utc.to_i)
+        end
+      end
+    end
+  end
+  def post_to_server(msg, init_timestamp)
+    puts "Sending message: #{msg}"
+    # Create the request object to use
+    uri, request = generate_http_obj(msg, init_timestamp)
+
+    # Set the options
+    req_options = { use_ssl: uri.scheme == 'https' }
+
+    begin
+      # Make the call
+      puts "making the call"
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      handle_response(response, init_timestamp)
+    rescue StandardError => _e
+      # @log.debug(e.message)
+    end
+  end
+
+  private
+
+
+
+
+
+  def generate_http_obj(msg, _init_timestamp)
+    uri = generate_uri
+    # @log.debug("(#{init_timestamp}) URL: #{uri}")
+
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = 'application/json; charset=utf-8'
+    request.body = data_prep(msg)
+    # @log.debug("(#{init_timestamp}) Body: #{request.body}")
+
+    [uri, request]
+  end
+
+  def generate_uri
+    base = determine_domain
+    path = determine_path
+    full_url = "#{base}#{path}"
+    URI.parse(full_url)
+  end
+
+  def determine_domain
+    domain_hash = {
+      'local': 'http://localhost:3000',
+      'staging': 'https://staging-api.pumptrakr.com',
+      'production': 'https://api.pumptrakr.com'
+    }
+    domain_hash[@environment.to_sym]
+  end
+
+  def determine_path
+    path_hash = {
+'GV600': '/api/v2/webhooks/modules/gv600_messages',
+'GV350': '/api/v2/webhooks/modules/gv350_messages'
+    }
+    path_hash[@model.to_sym]
+
+  end
+
+  def data_prep(msg)
+    { message: msg }.to_json
+  end
+
+  def handle_response(response, init_timestamp)
+    # Check the status code
+    if [200, 201, 204].include? response.code.to_i
+      handle_successful(response, init_timestamp)
+    else
+      handle_error(response, init_timestamp)
+    end
+  end
+
+  def handle_successful(response, init_timestamp)
+    # Everything worked
+    # @log.debug "(#{init_timestamp}) Successfully posted to PumpTrakr"
+    # @log.debug "(#{init_timestamp}) Code: #{response.code}"
+    # @log.debug "(#{init_timestamp}) Body: #{response.body}"
+  end
+
+  def handle_error(response, init_timestamp)
+    # Error!
+    # @log.debug "(#{init_timestamp}) Unsuccessfully posted to PumpTrakr"
+    # @log.debug "(#{init_timestamp}) Code: #{response.code}"
+    # @log.debug "(#{init_timestamp}) Body: #{response.body}"
+  end
+end
---
+++ test_messages.rb
@@ -0,0 +1,209 @@
+class Question
+  attr_accessor :prompt, :answer, :env
+  require 'socket'
+  require 'net/http'
+  require 'uri'
+  require 'json'
+  require 'logger'
+  require_relative 'proxy_test_msg'
+  require_relative 'command_line_checks'
+
+  def initialize(prompt, answer)
+    @prompt = prompt
+    @answer = answer
+
+  end
+end
+
+@port = 3333
+@module_model = ''
+@environment = ''
+@command = ''
+p1 = "Which Environment is the module on? \n\n1. Production \n2. Staging \n3. Exit"
+p2 = "What is the Model? \n\n1. GV600 \n2. GV350 \n3. Start Over \n4. Exit"
+p3 = "What message would you like to masquerade as the device, or: \n1. Start Over \n2. Exit"
+p4 = " WARNING: This will send a message from the TCP server as the device, and can not be undone, are you sure? \n\n1. Yes  \n2. Start Over \n3. Exit"
+
+
+@env_questions = [
+  Question.new(p1, ''),
+]
+
+@module_model_questions = [Question.new(p2, '')
+]
+
+@command_question = [Question.new(p3, '')]
+
+@confirm_question = [Question.new(p4, '')]
+
+def run_confirm(confirm_question)
+  confirm_question.each do |question|
+    puts question.prompt
+    answer = gets.chomp
+    case answer
+    when '1'
+      server = ProxyTestMsg.new(server_port = @port, environment = @environment, module_model = @module_model, msg = @command)
+      server.post_to_server(msg = @command, Time.now)
+      puts" \n\n
+      ==============================================
+        Message sent, check admin panel for status
+      ----------------------------------------------
+
+                     NEW SESSION STARTED
+
+      ----------------------------------------------\n\n"
+
+      run_cred(@env_questions)
+    when '2'
+      puts 'Message not sent, starting over'
+      run_cred(@env_questions)
+    when '3'
+      puts 'Message not sent, exiting'
+    else
+      puts 'Key stroke error, please try again valid responses are 1, 2 or 3'
+      run_confirm(@confirm_question)
+    end
+  end
+end
+
+def run_model(model_questions)
+  model_questions.each do |question|
+    puts question.prompt
+    answer = gets.chomp
+    case answer
+    when '1'
+      @module_model = "GV600"
+      puts "\n
+          ===========================================
+                       MESSAGE INFORMATION
+          ===========================================\n
+          -------------------------------------------------------------------------
+         |
+         |       Environment: #{@environment}
+         |       Module: #{@module_model}
+         |       Message to send: < not yet set >
+         |
+         |
+          -------------------------------------------------------------------------\n"
+      run_command(@command_question)
+    when '2'
+      @module_model = "GV350"
+      puts "\n
+          ===========================================
+                       MESSAGE INFORMATION
+          ===========================================\n
+          -------------------------------------------------------------------------
+         |
+         |       Environment: #{@environment}
+         |       Module: #{@module_model}
+         |       Message to send: < not yet set >
+         |
+         |
+          -------------------------------------------------------------------------\n"
+      run_command(@command_question)
+    when '3'
+      puts 'starting over'
+      run_cred(@env_questions)
+    when '4'
+      puts 'exiting -- bye'
+      exit
+    else
+      puts 'key stroke error, please try again valid responses are 1, 2, 3 or 4'
+      run_model(@module_model_questions)
+    end
+  end
+end
+
+def run_command(command_question)
+  command_question.each do |question|
+    puts question.prompt
+    answer = gets.chomp
+    case answer
+    when '1'
+      puts 'starting over'
+      run_cred(@env_questions)
+    when '2'
+      puts 'exiting -- bye'
+      exit
+    else
+      if answer.length > 20
+        @command = answer
+        puts "\n\n
+          ===========================================
+                       MESSAGE INFORMATION
+          ===========================================\n
+          -------------------------------------------------------------------------
+         |
+         |       Environment: #{@environment}
+         |       Module: #{@module_model}
+         |       Message to send: #{@command}
+         |       WARNING: Once the message is sent it can not be undone
+         |
+          -------------------------------------------------------------------------\n"
+        run_confirm(@confirm_question)
+      else
+        puts "\n
+          ===========================================
+                  !!!!!! == ERROR == !!!!!!
+          ===========================================\n
+          -------------------------------------------------------------------------
+         |
+         |       Environment: #{@environment}
+         |       Module: #{@module_model}
+         |       Message: #{answer}
+         |       {ERROR: Message must be greater than 20 characters - Exiting}
+         |
+          -------------------------------------------------------------------------\n"
+        exit
+      end
+    end
+  end
+end
+
+def run_cred(env_questions)
+
+  env_questions.each do |question|
+    puts question.prompt
+    answer = gets.chomp
+    case answer
+    when '1'
+      @environment = 'production'
+      puts "\n
+          ===========================================
+                       MESSAGE INFORMATION
+          ===========================================\n
+          -------------------------------------------------------------------------
+         |
+         |       Environment: #{@environment}
+         |       Module: < not yet set >
+         |       Message to send: < not yet set >
+         |
+         |
+          -------------------------------------------------------------------------\n"
+      run_model(@module_model_questions)
+    when '2'
+      @environment = 'staging'
+      puts "\n
+          ===========================================
+                       MESSAGE INFORMATION
+          ===========================================\n
+          -------------------------------------------------------------------------
+         |
+         |       Environment: #{@environment}
+         |       Module: < not yet set >
+         |       Message to send: < not yet set >
+         |
+         |
+          -------------------------------------------------------------------------\n"
+      run_model(@module_model_questions)
+    when '3'
+      puts 'exiting -- bye'
+      exit
+    else
+      puts 'key stroke error, please try again, valid responses are 1, 2, or 3'
+      run_cred(@env_questions)
+    end
+  end
+end
+
+run_cred(@env_questions)
